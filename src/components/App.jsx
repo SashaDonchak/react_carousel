ad214f7aa27caa964f91fab3a1f954dd9e9f8785
Sasha Donchak <pracadonchak@gmail.com>
2021-01-16 22:45:00 +0300

    Added main functionality (slides and swipes)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,19 @@ const App = () => (
     <>
         <div className="container">
             <h1>Hello, carousel!</h1>
-            <Carousel />
+            <Carousel>
+                <img src="./src/images/image1.jpg" alt="test" />
+                <div>
+                    <img src="./src/images/image2.jpg" alt="test" />
+                    <span>Wondeful!</span>
+                </div>
+                <div>
+                    test2
+                </div>
+                <div>
+                    test3
+                </div>
+            </Carousel>
         </div>
     </>
 )
--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,28 +1,79 @@
-import React, {useState} from 'react'
+import React, { useState, Children } from 'react'
 
-const Carousel = () => {
-    const [currentSlide, changeSlide] = useState(1);
-    const slidesCount = 7;
+const Carousel = ({ children }) => {
+    const [currentSlide, changeSlide] = useState(0);
+    const [pixelsToSlide, changePixels] = useState(0);
+    const slidesCount = Children.count(children);
+
+    let touchedX = 0;
+    let touchingX = 0;
 
     const goToPrevSlide = event => {
         event.preventDefault();
-        (currentSlide <= 1) 
-        ? changeSlide(slidesCount)
-        : changeSlide(currentSlide - 1);
+        
+        (currentSlide <= 0)
+            ? changeSlide(slidesCount-1)
+            : changeSlide(currentSlide - 1);
+    }
+
+    const handleTouchMove = event => {
+        const rect = event.target.getBoundingClientRect();
+        touchingX = event.touches[0].clientX - rect.left;
+
+        (touchedX > touchingX)
+        ? changePixels(touchedX - touchingX)
+        : changePixels(touchingX - touchedX)
+    }
+
+    const handleTouchEnd = event => {
+        event.preventDefault();
+
+        if (changePixels >= 100) {
+            (touchedX > touchingX)
+            ? goToNextSlide()
+            : goToPrevSlide()
+        }
+
+        changePixels(0);
+    }
+
+    const handleTouchStart = event => {
+        const rect = event.target.getBoundingClientRect();
+        touchedX = event.touches[0].clientX - rect.left;
     }
 
     const goToNextSlide = event => {
         event.preventDefault();
-        (currentSlide === slidesCount) 
-        ? changeSlide(1)
-        : changeSlide(currentSlide + 1);
+
+        (currentSlide === slidesCount-1)
+            ? changeSlide(0)
+            : changeSlide(currentSlide + 1);
+    }
+
+    const defaultStyle = {
+        width: `${400*slidesCount}px`,
+        transition: '0.3s',
+    }
+
+    console.log(pixelsToSlide)
+
+    const trackCurrentStyle = {
+        transform: `translate3d(-${200*(currentSlide)+pixelsToSlide}px, 0px, 0px)`,
     }
 
     return (
         <div className="carousel">
-            <div className="carousel-items">
-                <div className="carousel-item">
-                    {currentSlide}
+            <div className="carousel-items-list">
+                <div
+                    className="carousel-track"
+                    style={{...defaultStyle, ...trackCurrentStyle}}
+                    onTouchMove={handleTouchMove}
+                    onTouchStart={handleTouchStart}
+                    onTouchStart={handleTouchEnd}
+                >
+                    {Children.map(children, (child, i) => (
+                        <div className="carousel-item">{child}</div>
+                    ))}
                 </div>
             </div>
             <div className="carousel-nav">
