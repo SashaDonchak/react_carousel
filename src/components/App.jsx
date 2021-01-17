c4d1355937809d3b14e5d70ba8c2240a360ad505
Sasha Donchak <pracadonchak@gmail.com>
2021-01-17 13:36:31 +0300

    Some fixes with swipes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,22 @@
-import React from 'react'
-import Carousel from './Carousel'
-import './app.css'
+import React from 'react';
+import Carousel from './Carousel';
+import './app.css';
 
 const App = () => (
-    <>
-        <div className="container">
-            <h1>Hello, carousel!</h1>
-            <Carousel>
-                <img src="./src/images/image1.jpg" alt="test" />
-                <div>
-                    <img src="./src/images/image2.jpg" alt="test" />
-                    <span>Wondeful!</span>
-                </div>
-                <div>
-                    test2
-                </div>
-                <div>
-                    test3
-                </div>
-            </Carousel>
+  <>
+    <div className="container">
+      <h1>Hello, carousel!</h1>
+      <Carousel>
+        <img src="./src/images/image1.jpg" alt="test" />
+        <div>
+          <img src="./src/images/image2.jpg" alt="test" />
+          <span>Wondeful!</span>
         </div>
-    </>
-)
-
+        <div>test2</div>
+        <div>test3</div>
+      </Carousel>
+    </div>
+  </>
+);
 
 export default App;
--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,87 +1,82 @@
-import React, { useState, Children } from 'react'
+import React, { useState, Children } from 'react';
 
 const Carousel = ({ children }) => {
-    const [currentSlide, changeSlide] = useState(0);
-    const [pixelsToSlide, changePixels] = useState(0);
-    const slidesCount = Children.count(children);
+  const [currentSlide, changeSlide] = useState(0);
+  const [pixelsToSlide, changePixels] = useState(0);
+  const [touchedX, setTouchedPoint] = useState(0);
+  const [touchingX, setTouchingPoint] = useState(0);
+  const slidesCount = Children.count(children);
 
-    let touchedX = 0;
-    let touchingX = 0;
+  const goToPrevSlide = () => {
+    currentSlide <= 0
+      ? changeSlide(slidesCount - 1)
+      : changeSlide(currentSlide - 1);
+  };
 
-    const goToPrevSlide = event => {
-        event.preventDefault();
-        
-        (currentSlide <= 0)
-            ? changeSlide(slidesCount-1)
-            : changeSlide(currentSlide - 1);
+  const goToNextSlide = () => {
+    currentSlide === slidesCount - 1
+      ? changeSlide(0)
+      : changeSlide(currentSlide + 1);
+  };
+
+  const handleTouchMove = (event) => {
+    setTouchingPoint(event.touches[0].clientX);
+
+    touchedX > touchingX
+      ? changePixels(touchedX - touchingX)
+      : changePixels(touchingX - touchedX);
+  };
+
+  const handleTouchEnd = () => {
+    if (pixelsToSlide >= 10) {
+      touchedX > touchingX ? goToNextSlide() : goToPrevSlide();
     }
 
-    const handleTouchMove = event => {
-        const rect = event.target.getBoundingClientRect();
-        touchingX = event.touches[0].clientX - rect.left;
+    changePixels(0);
+  };
 
-        (touchedX > touchingX)
-        ? changePixels(touchedX - touchingX)
-        : changePixels(touchingX - touchedX)
-    }
+  const handleTouchStart = (event) => {
+    setTouchedPoint(event.touches[0].clientX);
+  };
 
-    const handleTouchEnd = event => {
-        event.preventDefault();
 
-        if (changePixels >= 100) {
-            (touchedX > touchingX)
-            ? goToNextSlide()
-            : goToPrevSlide()
-        }
 
-        changePixels(0);
-    }
+  const defaultStyle = {
+    width: `${400 * slidesCount}px`,
+    transition: '0.3s',
+  };
 
-    const handleTouchStart = event => {
-        const rect = event.target.getBoundingClientRect();
-        touchedX = event.touches[0].clientX - rect.left;
-    }
+  const trackCurrentStyle = {
+    transform: `translate3d(-${
+      200 * currentSlide - pixelsToSlide
+    }px, 0px, 0px)`,
+  };
 
-    const goToNextSlide = event => {
-        event.preventDefault();
-
-        (currentSlide === slidesCount-1)
-            ? changeSlide(0)
-            : changeSlide(currentSlide + 1);
-    }
-
-    const defaultStyle = {
-        width: `${400*slidesCount}px`,
-        transition: '0.3s',
-    }
-
-    console.log(pixelsToSlide)
-
-    const trackCurrentStyle = {
-        transform: `translate3d(-${200*(currentSlide)+pixelsToSlide}px, 0px, 0px)`,
-    }
-
-    return (
-        <div className="carousel">
-            <div className="carousel-items-list">
-                <div
-                    className="carousel-track"
-                    style={{...defaultStyle, ...trackCurrentStyle}}
-                    onTouchMove={handleTouchMove}
-                    onTouchStart={handleTouchStart}
-                    onTouchStart={handleTouchEnd}
-                >
-                    {Children.map(children, (child, i) => (
-                        <div className="carousel-item">{child}</div>
-                    ))}
-                </div>
-            </div>
-            <div className="carousel-nav">
-                <button className="carousel-nav__prev" onClick={goToPrevSlide}>Prev</button>
-                <button className="carousel-nav__next" onClick={goToNextSlide}>Next</button>
-            </div>
+  return (
+    <div className="carousel">
+      <div className="carousel-items-list">
+        <div
+          className="carousel-track"
+          style={{ ...defaultStyle, ...trackCurrentStyle }}
+          onTouchMove={handleTouchMove}
+          onTouchStart={handleTouchStart}
+          onTouchEnd={handleTouchEnd}
+        >
+          {Children.map(children, (child, i) => (
+            <div className="carousel-item">{child}</div>
+          ))}
         </div>
-    )
-}
+      </div>
+      <div className="carousel-nav">
+        <button className="carousel-nav__prev" onClick={goToPrevSlide}>
+          Prev
+        </button>
+        <button className="carousel-nav__next" onClick={goToNextSlide}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Carousel;
